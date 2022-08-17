dcd2f3ca41a4d87b6aa497165da7f3efd83e4f66
viniciushrcs <viniciushrcs@gmail.com>
2022-08-17 20:04:44 -0300

    chore: organize service folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesController } from './coffees/coffees.controller';
-import { CoffeeService } from './coffee/coffee.service';
+import { CoffeeService } from './coffees/coffee.service';
 
 @Module({
   imports: [],
