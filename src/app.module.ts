f104fcc5ed5fb11539265d1355142288d418acb8
viniciushrcs <viniciushrcs@gmail.com>
2022-08-17 19:46:07 -0300

    feat: adds coffee service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesController } from './coffees/coffees.controller';
+import { CoffeeService } from './coffee/coffee.service';
 
 @Module({
   imports: [],
   controllers: [AppController, CoffeesController],
-  providers: [AppService],
+  providers: [AppService, CoffeeService],
 })
 export class AppModule {}
