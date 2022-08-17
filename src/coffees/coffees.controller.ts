86d6291e263cbab05e39cf086cab3c846ce551b1
viniciushrcs <viniciushrcs@gmail.com>
2022-08-17 20:05:33 -0300

    feat: integrates coffees controller and service and cretaes coffee entity

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,7 +1,18 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Patch,
+  Delete,
+} from '@nestjs/common';
+import { CoffeeService } from './coffee.service';
 
 @Controller('coffees')
 export class CoffeesController {
+  constructor(private readonly coffeeService: CoffeeService) {}
+
   @Get()
   findAll() {
     return 'This route will return all coffees';
@@ -9,11 +20,21 @@ export class CoffeesController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return `This route return coffee's ${id}`;
+    return this.coffeeService.findOne(id);
   }
 
   @Post()
   create(@Body() body: any) {
-    return body;
+    return this.coffeeService.create(body);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return this.coffeeService.update(id, body);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.coffeeService.remove(id);
   }
 }
